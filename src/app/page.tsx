9fa1bb493b2694665c44968b275fad3634066a81
syed_huzaifa <shuzaifa32@gmail.com>
2024-11-04 23:34:48 +0500

    Vercel_clone-App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,81 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Login_page (){
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <div className="bg-[#0a0a0a] border-b border-b-[#24292e]">
+        <header className="flex justify-between text-white ">
+          <div className="mx-4 mt-2">
+            <button>
+            <Image src="/favicon.ico" alt="loading" width={50} height={80}></Image>
+            </button>
+          </div>
+          <div className="flex items-center mx-4 my-2 space-x-1">
+            <button className="w-20 h-fit p-1 text-[#555] font-bold">Contact</button>
+            <button className="border border-[#24292e] rounded-lg w-20 h-fit p-1 font-bold hover:bg-[#555]">Sign Up</button>
+          </div>
+        </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="text-white my-8 text-center">
+
+          <h1 className="text-3xl font-bold text-gray-200">Log in to Vercel</h1>
+
+          <div className="flex flex-col items-center">
+
+          <button className="bg-[#24292e] hover:bg-[#555] mt-4 p-3 font-bold rounded-lg w-80 border border-[#24292e] hover:border-[#555]">Continue with GitHub</button>
+
+          <button className="bg-[#6b4fbb] hover:bg-[#8367D3] mt-2 p-3 font-bold rounded-lg w-80 border border-[#6b4fbb] hover:border-[#8367D3]">Continue with GitLab</button>
+
+          <button className="bg-[#0052CC] hover:bg-[#1668E2] mt-2 p-3 font-bold rounded-lg w-80 border border-[#0052CC] hover:border-[#1668E2]">Continue with BitBucket</button>
+          
+          <hr className="mt-5 mb-4 w-80 border-[#24292e] "/>
+
+          <button className=" hover:bg-[#555] mt-2 p-3 font-bold rounded-lg w-80 border border-[#24292e] hover:border-[#555]">Continue with SAML SSO</button>
+
+          <button className=" hover:bg-[#555] mt-2 p-3 font-bold rounded-lg w-80 border border-[#24292e] hover:border-[#555]">Login with PassKey</button>
+
+          <a href="" className="mt-5 mb-2 text-[#1668E2] hover:underline">Continue with Email?</a>
+
+          </div>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className="text-center border-b border-[#24292e]"> 
+      <a href="" className=" text-[#1668E2] hover:underline py-10 block">Continue with Email?Sign Up</a>
+      </div>
+      
+     <div className="text-white">
+      <div className="mx-24 my-8 flex justify-between">
+        <div className="flex space-x-3 p-1">
+          <p>@ 2024</p>
+          <button className="text-[#1668E2] hover:bg-[#24292e] hover:rounded-md"><li>All systems normal</li></button>
+        </div>
+        <div>
+          <button className="hover:bg-[#24292e] hover:rounded-md text-[#555]">Command Menu</button>
+        </div>
+      </div>
+      <div className="flex justify-between mx-24 my-5" >
+        <div className="space-x-6">
+          <button>git</button>
+          <button>twit</button>
+        </div>
+        <div>
+          <ul className="flex space-x-12">
+          <li className="cursor-pointer text-[#555]  hover:text-white">Home</li>
+          <li className="cursor-pointer text-[#555]  hover:text-white">Documentation</li>
+          <li className="cursor-pointer text-[#555]  hover:text-white">Guides</li>
+          <li className="cursor-pointer text-[#555]  hover:text-white">Help</li>
+          <li className="cursor-pointer text-[#555]  hover:text-white">Contact Sale</li>
+          <li className="cursor-pointer text-[#555]  hover:text-white">Blog</li>
+          <li className="cursor-pointer text-[#555]  hover:text-white">Changelog</li>
+          <li className="cursor-pointer text-[#555]  hover:text-white">Pricing</li>
+          <li className="cursor-pointer text-[#555]  hover:text-white">Enterprise</li>
+          <li className="cursor-pointer text-[#555]  hover:text-white">Legal</li>
+          </ul>
+        </div>
+      </div>
+     </div>
     </div>
-  );
+  )
 }
